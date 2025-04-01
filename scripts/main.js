74740dc70f84c9cb39177a7aa1fc03af66cb9e5a
Preethi <preethishetty636604@gmail.com>
2025-04-01 18:44:16 +0530

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+// JavaScript file 
+// main.js (inside the scripts folder)
+
+document.querySelector('a-entity').addEventListener('click', function() {
+    console.log('3D object clicked!');
+});
+
